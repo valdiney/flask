9ef7c1527b94d382e123a31ecfb6e990a31582a1
valdiney <valdiney.2@hotmail.com>
2018-02-18 21:54:35 -0300

    terceiro commit

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template('views/dashboard/index.html')
+
+
+@app.route("/sistema_linguagem")
+def sistema_de_linguagem():
+    return render_template('views/teoria/sistema_de_linguagem.html')
 
 
 app.run()
